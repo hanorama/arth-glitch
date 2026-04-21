43981a85593f214dbcfce95d28db9973c90ab3b1
Hannah Kostuk <sak675@usask.ca>
2026-04-20 20:13:02 -0600

    added main.py, with comments outlining the main programming steps

---
+++ main.py
@@ -0,0 +1,8 @@
+# -- PLAN -- 
+# choose a photo to upload
+# read photo as text
+# parse through photo, making selections/replacements/removals based on selection method
+# execute through photo text in an iterative fashion, saving a copy of the photo text every so often
+# then, save the series of images to disk
+# if possible, turn series of images into a video and save THAT to disk instead/as well
+
