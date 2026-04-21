5ba69eb3dfc5ae5576f64d2588d2e606eddebe74
Hannah Kostuk <sak675@usask.ca>
2026-04-20 20:17:32 -0600

    updated TO-DO list in main.py, separated into basic pre-programming and programming steps

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-# -- PLAN -- 
+# TO - DO :
+
+# -- PRE-PROGRAMMING --
+# visit source sites again
+# check out if anyone has done work like this before - if applicable, learn from their process
+# choose 2-3 selection algorithms
+# find + choose a good image processing library for python
+#
+
+# -- PROGRAMMING -- 
 # choose a photo to upload
 # read photo as text
 # parse through photo, making selections/replacements/removals based on selection method
@@ -6,3 +15,4 @@
 # then, save the series of images to disk
 # if possible, turn series of images into a video and save THAT to disk instead/as well
 
+# end of to - do.
