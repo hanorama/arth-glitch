08910c65ae5715adb632089f8b3467a0cdfe94c6
Hannah Kostuk <sak675@usask.ca>
2026-04-22 15:59:21 -0600

    added functionality for finding the header of the image file, so that we can only edit the image after the header. This is necessary so that the file doesnt corrupt and becoming unviewable after saving.

--- main.py
+++ main.py
@@ -14,17 +14,44 @@
 # if possible, turn series of images into a video and save THAT to disk instead/as well
 
 # end of to - do.
+
+# welcome + file input
 print("Welcome to the glitch generator.")
 print("Please enter the name of the image to glitch, including the file extension (.jpg, .png, etc.).")
 print("If you do not want to add an image to this repo, leave the input blank and press enter to use a preset.")
 IMG_TO_GLITCH = input("")
 if (IMG_TO_GLITCH == ""):
     IMG_TO_GLITCH = "real5.jpg"
+    #IMG_TO_GLITCH = "wisdomteeth.png"
 
 # open the binary data of the image
 with open(IMG_TO_GLITCH, 'rb') as in_data:
     in_data = in_data.read() # reads in an object of class bytes
-    ba = bytearray(in_data) # converts to a bytearray so we can modify the bytes directly
+    ba = bytearray(in_data) # converts to a bytearray so we can modify the bytes directly. prints as hexadecimal (\x00)
+
+# keep the header of the image safe
+# if we don't, then it won't be able to convert back to an image
+# jpg and jpeg headers end with \xff\xda
+ba_length = len(ba)
+
+#get SOS index
+for i in range(ba_length):
+    current_hex = ba[i]
+    if i < ba_length:
+        next_hex = ba[i+1]
+        if (current_hex == 0xff and next_hex == 0xda): ## \xff and \xda
+            SOS_index = i + 2 # Start Of Scan index (where the image starts, after the header ends)
+            print("SOS Index:" + str(SOS_index))
+            break #found end of header, now exit the loop
+
+# modify the image, ex) ba[200] = 0x00
+print(ba[SOS_index])
+for i in range(SOS_index, ba_length): # from SOS to end of file:
+    pass
+
+# linear search starting at 0
+# linear search starting at length
+# replace every number with 1
 
 # save the modified binary data as a copy of the image
 with open("glitched_" + IMG_TO_GLITCH, 'wb') as out_data:
