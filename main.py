30de5e919e27568c057eb7f77e0be621cc81c248
Hannah Kostuk <sak675@usask.ca>
2026-04-22 14:47:16 -0600

    added request for user input, so the user can choose the photo to glitch. Also provides an option to not input any text and use a default instead

--- main.py
+++ main.py
@@ -14,17 +14,19 @@
 # if possible, turn series of images into a video and save THAT to disk instead/as well
 
 # end of to - do.
-
-real_in_data = []
+print("Welcome to the glitch generator.")
+print("Please enter the name of the image to glitch, including the file extension (.jpg, .png, etc.).")
+print("If you do not want to add an image to this repo, leave the input blank and press enter to use a preset.")
+IMG_TO_GLITCH = input("")
+if (IMG_TO_GLITCH == ""):
+    IMG_TO_GLITCH = "real5.jpg"
 
 # open the binary data of the image
-with open('real5.jpg', 'rb') as real_in:
-    real_in_data = real_in.read() # reads in an object of class bytes
-    real_in_data = real_in_data.decode() # casts from byte to string type
-    print(type(real_in_data))
-
+with open(IMG_TO_GLITCH, 'rb') as in_data:
+    in_data = in_data.read() # reads in an object of class bytes
+    ba = bytearray(in_data) # converts to a bytearray so we can modify the bytes directly
 
 # save the modified binary data as a copy of the image
-with open('real5MODDED.jpg', 'wb') as real_out:
-    real_in_data = real_in_data.encode() # cast back from string to byte type
-    real_out.write(real_in_data)
+with open("glitched_" + IMG_TO_GLITCH, 'wb') as out_data:
+    out_data.write(ba)
+    print("Glitch complete.")
